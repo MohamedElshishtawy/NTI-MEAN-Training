d8b23642dca94db50b15c65bb5006aadaaa27a89
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-07-15 15:14:10 +0300

    Fix dropdown options display

--- task-4/main.js
+++ task-4/main.js
@@ -1,22 +1,21 @@
-TaskType = {
-    TASK: {
+TaskType = [
+    {
         name: "TasK",
         color: "gray",
     },
-    IN_PROGRESS: {
+     {
         name: "In Progress",
         color: "blue",
     },
-    DONE: {
+     {
         name: "Done",
         color: "green",
     }
-}
+]
 let TypeSelector = document.getElementById("type")
 for (let i = 0; i < TaskType.length; i++) {
     let option = document.createElement("option");
     option.value = TaskType[i].name;
     option.textContent = TaskType[i].name;
-    option.style.color = TaskType[i].color;
     TypeSelector.appendChild(option);
 }
