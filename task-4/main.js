b8a5e7f7068cfc9a5e3344babd2763266c159508
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-07-15 15:24:56 +0300

    add the the form listener

--- task-4/main.js
+++ task-4/main.js
@@ -12,10 +12,42 @@ TaskType = [
         color: "green",
     }
 ]
+
+
 let TypeSelector = document.getElementById("type")
+
 for (let i = 0; i < TaskType.length; i++) {
     let option = document.createElement("option");
     option.value = TaskType[i].name;
     option.textContent = TaskType[i].name;
     TypeSelector.appendChild(option);
 }
+
+TaskForm = document.getElementById("taskForm");
+
+
+TasksList = [];
+
+TaskForm.addEventListener("submit", function(event) {
+   
+    event.preventDefault();
+
+    let nameInput = document.getElementById("name");
+    let typeInput = document.getElementById("type");
+
+    // console.log(nameInput)
+
+    TasksList.push({
+        name: nameInput.value,
+        type: typeInput.value,
+        createdAt: new Date()
+    });
+
+    nameInput.value = "";
+    typeInput.value = TaskType[0].name;
+
+    console.log(TasksList);
+
+});
+
+
